4d63d37c439e428a3fcfd19497392affb0d81998
I-the-Creator <andrey.burluckiy@gmail.com>
2022-04-04 21:13:33 +0200

    general routing added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
+
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import Onboarding from './pages/Onboarding'
 
 function App() {
   return (
-    <div className="App">
-      Hello, world
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/dashboard" element={<Dashboard />}/>
+        <Route path="/onboarding" element={<Onboarding />}/>
+      </Routes>
+    </BrowserRouter>
+    
   );
 }
 
