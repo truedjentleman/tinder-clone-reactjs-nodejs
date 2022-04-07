a4f36b54545338f75aba3ff127f66b5e03cde937
I-the-Creator <andrey.burluckiy@gmail.com>
2022-04-07 10:09:10 +0200

    home component updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,29 @@
-const Home = () => {
-    return (
-        <div>
-            Home
-        </div>
-    )
-}
+import { useState } from "react";
+import AuthModal from "../components/AuthModal";
 
-export default Home
+import Nav from "../components/Nav";
+
+const Home = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const authToken = false;
+
+  const handleClick = () => {
+    setShowModal(true);
+  };
+
+  return (
+    <div className="overlay">
+      <Nav minimal={false} authToken={authToken} setShowModal={setShowModal} showModal={showModal} />
+      <div className="home">
+        <h1>Swipe Right®</h1>
+        <button className="primary-button" onClick={handleClick}>
+          {authToken ? "Signout" : "Create Account"}
+        </button>
+        {showModal && <AuthModal setShowModal={setShowModal} />}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
