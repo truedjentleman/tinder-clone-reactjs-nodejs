192c81e2f3d684e9374520d4f62af8a5a052f965
I-the-Creator <andrey.burluckiy@gmail.com>
2022-04-14 22:29:27 +0200

    dashboard components created

---
+++ client/src/components/ChatContainer.js
@@ -0,0 +1,22 @@
+import ChatDisplay from "./ChatDisplay"
+import ChatHeader from "./ChatHeader"
+import MatchesDisplay from "./MatchesDisplay"
+
+const ChatContainer = () => {
+    return (
+        <div className="chat-container">
+            <ChatHeader/>
+
+            <div>
+                <button className="option">Matches</button>
+                <button className="option">Chat</button>
+            </div>
+
+            <MatchesDisplay/>
+
+            <ChatDisplay/>
+        </div>
+    )
+}
+
+export default ChatContainer
---
+++ client/src/components/ChatHeader.js
@@ -0,0 +1,16 @@
+
+const ChatHeader = () => {
+    return (
+        <div className="chat-container-header">
+            <div className="profile">
+                <div className="image-container">
+                    <img src="" alt=""/>
+                </div>
+                <h3>User Name</h3>
+            </div>
+            <i className="log-out-icon">⇦</i>
+        </div>
+    );
+};
+
+export default ChatHeader;
