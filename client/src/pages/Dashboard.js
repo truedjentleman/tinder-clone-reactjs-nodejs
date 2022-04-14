3009ac7d2e90c769a74c0837a7ed47eb145005c4
I-the-Creator <andrey.burluckiy@gmail.com>
2022-04-14 22:31:18 +0200

    all front-end pages and components moved to dedicated folder 'client'

---
+++ client/src/pages/Dashboard.js
@@ -0,0 +1,69 @@
+import TinderCard from "react-tinder-card";
+import { useState } from "react";
+
+import ChatContainer from "../components/ChatContainer"
+
+const Dashboard = () => {
+  const characters = [
+    {
+      name: "Richard Hendricks",
+      url: "https://imgur.com/Q9WPlWA.jpg",
+    },
+    {
+      name: "Erlich Bachman",
+      url: "https://imgur.com/Q9WPlWA.jpg",
+    },
+    {
+      name: "Monica Hall",
+      url: "https://imgur.com/Q9WPlWA.jpg",
+    },
+    {
+      name: "Jared Dunn",
+      url: "https://imgur.com/Q9WPlWA.jpg",
+    },
+    {
+      name: "Dinesh Chugtai",
+      url: "https://imgur.com/Q9WPlWA.jpg",
+    },
+  ];
+  const [lastDirection, setLastDirection] = useState();
+
+  const swiped = (direction, nameToDelete) => {
+    console.log("removing: " + nameToDelete);
+    setLastDirection(direction);
+  };
+
+  const outOfFrame = (name) => {
+    console.log(name + " left the screen!");
+  };
+
+  return (
+    <div className="dashboard">
+      <ChatContainer/>
+      <div className="swipe-container">
+        <div className="card-container">
+          {characters.map((character) => 
+            <TinderCard
+              className="swipe"
+              key={character.name}
+              onSwipe={(dir) => swiped(dir, character.name)}
+              onCardLeftScreen={() => outOfFrame(character.name)}
+            >
+              <div
+                style={{ backgroundImage: "url(" + character.url + ")" }}
+                className="card"
+              >
+                <h3>{character.name}</h3>
+              </div>
+            </TinderCard>
+          )}
+          <div className="swipe-info">
+              {lastDirection ? <p>You swiped {lastDirection}</p> : <p></p>}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
