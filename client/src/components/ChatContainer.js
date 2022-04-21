b7e386b9f262c336db4c9a0198fa54a53575657b
I-the-Creator <andrey.burluckiy@gmail.com>
2022-04-21 10:07:15 +0200

    prop 'user' drilled, logout function added to ChatHeader

--- client/src/components/ChatContainer.js
+++ client/src/components/ChatContainer.js
@@ -2,10 +2,11 @@ import ChatDisplay from "./ChatDisplay"
 import ChatHeader from "./ChatHeader"
 import MatchesDisplay from "./MatchesDisplay"
 
-const ChatContainer = () => {
+const ChatContainer = ({ user }) => {
+
     return (
         <div className="chat-container">
-            <ChatHeader/>
+            <ChatHeader user={user}/>
 
             <div>
                 <button className="option">Matches</button>
--- client/src/components/ChatHeader.js
+++ client/src/components/ChatHeader.js
@@ -1,14 +1,29 @@
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
 
-const ChatHeader = () => {
+const ChatHeader = ({ user }) => {
+    const [cookies, setCookie, removeCookie] = useCookies(["user"]);
+
+    const navigate = useNavigate();
+
+    const logout = () => {
+        removeCookie('UserId', cookies.UserId)
+        removeCookie('AuthToken', cookies.AuthToken)
+        navigate("/");
+        window.location.reload() // reload the window after log out
+
+    }
+
     return (
         <div className="chat-container-header">
             <div className="profile">
                 <div className="image-container">
-                    <img src="" alt=""/>
+                    <img src={user.url} alt={'photo of ' + user.first_name}/>
                 </div>
-                <h3>User Name</h3>
+                <h3>{user.first_name}</h3>
             </div>
-            <i className="log-out-icon">⇦</i>
+            {/* remove Cookies on click Log Out button */}
+            <i className="log-out-icon" onClick={logout}>⇦</i>
         </div>
     );
 };
