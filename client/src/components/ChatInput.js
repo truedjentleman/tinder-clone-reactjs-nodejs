77f2402b6eebf0f22b7e5f6fab8f4e9a7dc35f6e
I-the-Creator <andrey.burluckiy@gmail.com>
2022-04-29 13:52:54 +0200

    add function to post new message and get all previous messages from clicked user; chat layout updated to get new values

--- client/src/components/ChatInput.js
+++ client/src/components/ChatInput.js
@@ -1,13 +1,37 @@
+import axios from 'axios';
 import { useState } from 'react';
 
 
-const ChatInput = () => {
-    const [textArea, setTextArea] = useState(null)
+const ChatInput = ({ user, clickedUser, getUsersMessages, getClickedUsersMessages }) => {
+    const [textArea, setTextArea] = useState("")
+    const userId = user?.user_id
+    const clickedUserId = clickedUser?.user_id
+
+    const addMessage = async () => {
+        const message = {
+            timestamp: new Date().toISOString(),
+            from_userId: userId,
+            to_userId: clickedUserId,
+            message: textArea
+        }
+        
+        // console.log(message);  // DEBUG
+
+        try {
+            await axios.post('http://localhost:8000/message', {message})
+            getUsersMessages()  // get all previous user's messages 
+            getClickedUsersMessages() // get all previous clicked user's messages 
+            setTextArea("")  // reset text Area
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    
 
     return (
         <div className="chat-input">
             <textarea value={textArea} onChange={(e) => setTextArea(e.target.value)}/>  
-            <button className="secondary-button">Submit</button>       
+            <button className="secondary-button" onClick={addMessage}>Submit</button>       
         </div>
     );
 };
