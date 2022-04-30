f00ae25a8e68923e405c8db41de5ec83ae67118d
I-the-Creator <andrey.burluckiy@gmail.com>
2022-04-30 18:54:55 +0200

    add logic to clear cookies and reload page if 'Sign Out' button clicked; works only if user authorized

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,22 +1,33 @@
 import { useState } from "react";
 import AuthModal from "../components/AuthModal";
+import { useCookies } from "react-cookie";
+
 
 import Nav from "../components/Nav";
 
 const Home = () => {
   const [showModal, setShowModal] = useState(false);
   const [isSignUp, setIsSignUp] = useState(true);
-
-  const authToken = false;
+  const [cookies, setCookie, removeCookie] = useCookies(["user"]);
+  
+  const authToken = cookies.AuthToken;
 
   const handleClick = () => {
+    if(authToken) {
+      removeCookie('UserId', cookies.UserId)
+      removeCookie('AuthToken', cookies.AuthToken)
+      window.location.reload();
+      return
+    }
+
     setShowModal(true);
-    setIsSignUp(true);  // reset isSignUp state to defaulf 'true' value after closing authModal
+    setIsSignUp(true);  // reset isSignUp state to default 'true' value after closing authModal
   };
 
   return (
     <div className="overlay">
       <Nav
+        authToken={authToken}
         minimal={false}
         setShowModal={setShowModal}
         showModal={showModal}
